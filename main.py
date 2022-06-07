48b649b8d0d73fbf5293899ad97935c6b647cf7d
T-Wesst <tyroojwest@gmail.com>
2022-06-07 00:17:59 -0700

    remove unused imports and format

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
 import os, googlemaps, json
 from dotenv import load_dotenv, find_dotenv
-from pprint import pp
 
 load_dotenv(find_dotenv())
 API_KEY = os.getenv('API_KEY')
 mapClient = googlemaps.Client(key=API_KEY)
 
-def searchFor(category):
-  response = mapClient.places(query=category)
-  return response.get('results')
 
-def writeBusinessDetails(listOfBusinesses, category):
-  with open(f'./categories/{category}.json', 'w') as detailsFile:
-    json.dump(listOfBusinesses, detailsFile)
+def searchFor(category):
+    response = mapClient.places(query=category)
+    return response.get('results')
+
+
+def writeToFile(listOfBusinesses, categoryFile):
+    with open(f'./categories/{categoryFile}.json', 'w') as detailsFile:
+        json.dump(listOfBusinesses, detailsFile)
+
 
 def getAdditionalDetails(businessID):
-  details = mapClient.place(place_id=businessID)
-  phone = details["result"]["formatted_phone_number"]
-  # website = details["result"]["website"]
-  return phone
+    details = mapClient.place(place_id=businessID)
+    phone = details["result"]["formatted_phone_number"]
+    # website = details["result"]["website"]
+    return phone
 
-input = "pet-groomer"
-results = searchFor(input)
+
+category = "pet-groomer"
+results = searchFor(category)
 
 businesses = []
 for result in results:
-  business = {
-    'identifier': result['place_id'],
-    'status': result['business_status'],
-    'title': result['name'],
-    'address': result['formatted_address'],
-  }
-  business['phone'] = getAdditionalDetails(business['identifier'])
-  businesses.append(business)
-writeBusinessDetails(businesses, input)
+    business = {
+        'identifier': result['place_id'],
+        'status': result['business_status'],
+        'title': result['name'],
+        'address': result['formatted_address'],
+    }
+    business['phone'] = getAdditionalDetails(business['identifier'])
+    businesses.append(business)
+writeToFile(businesses, category)
